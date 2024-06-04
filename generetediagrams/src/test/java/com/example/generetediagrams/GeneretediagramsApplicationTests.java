3117bc0c194eef783880faffe0c0db58074a45dc
Emanuel Bissacotti <emanuelfbissacotti@gmail.com>
2024-06-04 15:24:14 -0300

    Adiciona estutura inicial da aplicação
    Inicializa spring boot.

---
+++ generetediagrams/src/test/java/com/example/generetediagrams/GeneretediagramsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.generetediagrams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneretediagramsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
